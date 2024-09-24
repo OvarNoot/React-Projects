73c5112a9281d79414956d74e8186d4dbcec7fe3
hh35 <4951hh@gmail.com>
2024-09-24 14:28:36 -0400

    Accordian

---
+++ src/components/accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id : '1',
+        question: "When was React Intorduced",
+        answer: "2013"
+    },
+    {
+        id : '2',
+        question: "Is React open source?",
+        answer: "Yes"
+    },
+    {
+        id : '3',
+        question: "Can you use React to develop single page application?",
+        answer: "Yuh"
+    },
+    {
+        id : '4',
+        question: "What do I like in react",
+        answer: "Resuable Components"
+    }
+];
+
+export default data;
